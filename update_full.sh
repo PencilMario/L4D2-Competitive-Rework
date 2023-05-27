63752a0ad3e93f6866a510aa46641d947aaef8a3
PencilMario <Sir.P@foxmail.com>
2023-05-27 18:34:59 +0800

    开局music

--- update_full.sh
+++ update_full.sh
@@ -10,7 +10,12 @@ directories=("/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" "/home/s
 
 for dir in "${directories[@]}"; do
     if [ -d "$dir" ]; then
-        find "$dir/addons/sourcemod/" ! -path "$dir/addons/sourcemod/logs*" ! -path "$dir/addons/sourcemod/configs/sourcebans/sourcebans.cfg" ! -path "$dir/addons/sourcemod/configs/databases.cfg" -type f -delete        
+        find "$dir/addons/sourcemod/" \
+            ! -path "$dir/addons/sourcemod/logs*" \
+            ! -path "$dir/addons/sourcemod/configs/sourcebans/sourcebans.cfg" \
+            ! -path "$dir/addons/sourcemod/configs/databases.cfg" \
+            ! -path "$dir/addons/sourcemod/data/music_mapstart.txt" \
+            -type f -delete        
 
         rm -rf "$dir/addons/metamod/"*
         rm -rf "$dir/addons/l4dtoolz/"*
