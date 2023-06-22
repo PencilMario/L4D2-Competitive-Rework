f7dae43cffc7307b6ddaa413bd20d1495c9310f4
PencilMario <Sir.P@foxmail.com>
2023-06-22 10:52:57 +0800

    ??

--- update_full.sh
+++ update_full.sh
@@ -12,7 +12,7 @@ for dir in "${directories[@]}"; do
     if [ -d "$dir" ]; then
         find "$dir/addons/sourcemod/" \
             ! -path "$dir/addons/sourcemod/logs*" \
-            ! -path "$dir/addons/sourcemod/configs/admins_simple.ini \
+            ! -path "$dir/addons/sourcemod/configs/admins_simple.ini" \
             ! -path "$dir/addons/sourcemod/configs/sourcebans*" \
             ! -path "$dir/addons/sourcemod/configs/databases.cfg" \
             ! -path "$dir/addons/sourcemod/data/music_mapstart.txt" \
