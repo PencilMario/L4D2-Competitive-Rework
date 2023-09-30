5aef173fcc9c5ede15c5842e050dfea122ade802
PencilMario <Sir.P@foxmail.com>
2023-09-30 10:48:21 +0800

    服务器更新

--- Dedicated Server Install Guide/auto_add_to_favserver/output.py
+++ Dedicated Server Install Guide/auto_add_to_favserver/output.py
@@ -7,6 +7,8 @@ data.append(("dx1.nekoyun.cn",34107))
 data.append(("dx1.nekoyun.cn",34108))
 data.append(("dx1.nekoyun.cn",34109))
 data.append(("dx1.nekoyun.cn",34110))
+data.append(("sq.xubw.cn",28013))
+data.append(("sq.xubw.cn",28014))
 
 
 with open('nb_servers.json', 'w', encoding = 'utf-8') as files:
