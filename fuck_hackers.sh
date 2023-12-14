1e2f5672b30ae0a3b4e81212d64dce72b72b5651
PencilMario <Sir.P@foxmail.com>
2023-12-14 20:01:30 +0800

    :)

--- fuck_hackers.sh
+++ fuck_hackers.sh
@@ -3,6 +3,12 @@
 # IP列表
 IP=("120.225.14.155")
 
+sudo iptables -F
+sudo iptables -X
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+
 # 封禁IP
 for i in "${IP[@]}"
 do
